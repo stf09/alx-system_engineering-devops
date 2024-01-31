e16de3a0b89a1d0915cb731cf9d09406b7fc821c
stf09 <bikemlit32@gmail.com>
2024-01-31 09:27:34 -0800

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,36 +1,14 @@
 #!/usr/bin/python3
-"""script for parsing web data from an api
-"""
+"""Returns to-do list information for a given employee ID."""
+import requests
+import sys
+
 if __name__ == "__main__":
-    import json
-    import requests
-    import sys
-    base_url = 'https://jsonplaceholder.typicode.com/'
-    try:
-        employee_id = sys.argv[1]
-    except:
-        print('Usage: {} employee_id'.format(sys.argv[0]))
-        exit(1)
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    # grab the info about the user
-    url = base_url + 'users?id={}'.format(employee_id)
-    response = requests.get(url)
-    user = json.loads(response.text)
-    name = user[0].get('name')
-
-    # grab the info about the user's tasks
-    url = base_url + 'todos?userId={}'.format(employee_id)
-    response = requests.get(url)
-    objs = json.loads(response.text)
-    completed = 0
-    completed_tasks = []
-    for obj in objs:
-        if obj.get('completed'):
-            completed_tasks.append(obj)
-            completed += 1
-
-    # print the output about user's task completion
-    print("{} is done with tasks({}/{}):".format(name, completed, len(objs)))
-    # print the output title of completed tasks
-    for task in completed_tasks:
-        print("\t {}".format(task.get('title')))
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
