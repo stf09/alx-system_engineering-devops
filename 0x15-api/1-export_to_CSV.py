9fbee3eb7dd626f012b953909ddc66d6b6a5fa6c
stf09 <bikemlit32@gmail.com>
2024-01-31 10:05:03 -0800

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(user_id)).json()
-    print(user)
     username = user.get("username")
     print(username)
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
@@ -17,5 +16,5 @@ if __name__ == "__main__":
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
+            [user_id, (requests.get(url + "users/{}".format(user_id)).json()).get("username"), t.get("completed"), t.get("title")]
          ) for t in todos]
